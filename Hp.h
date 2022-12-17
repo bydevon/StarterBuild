87e5f4efbe61a5deec9f7322de6ca69c087ddc4e
bydevon <manikmodz123@gmail.com>
2022-12-17 02:54:08 -0800

    Add project files.

---
+++ Hp.h
@@ -0,0 +1,13 @@
+#pragma once
+#include <cstdint>
+
+
+class hp {
+public:
+
+private:
+	std::uint32_t MaxHP;
+	std::uint32_t CurrentHP;
+	std::uint32_t ShieldHP;
+};
+
---
+++ Hptypes.h
@@ -0,0 +1,4 @@
+#pragma once
+#include <cstdint>
+
+typedef std::int32_t hptype;
